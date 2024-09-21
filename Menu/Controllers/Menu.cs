0cd6011a1739f9c068f5b45044e05be7695455f4
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2024-09-21 10:42:05 +0530

    Add search functionality to Menu and update views
    The `Index` method in `MenuController` now supports searching for dishes by name. If a `searchString` is provided, it filters the dishes accordingly before returning the view.
    
    Minor formatting adjustments were made in the `Details.cshtml` view to improve code readability.
    
    A search form was added to the navigation bar in `_Layout.cshtml`, enabling users to search for dishes directly from the main layout. The form includes a text input for the search string and a submit button.

--- Menu/Controllers/Menu.cs
+++ Menu/Controllers/Menu.cs
@@ -12,8 +12,17 @@ namespace Menu.Controllers
         public Menu(MenuContext context) {
 			_context = context;
 		}
-		public async Task<IActionResult> Index()
+		public async Task<IActionResult> Index(string searchString)
         {
+			var dishes  =  from d in  _context.Dishes
+						   select d;
+
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				dishes = dishes.Where(d=>d.Name.Contains(searchString));
+				return View(await dishes.ToListAsync());
+			}
+
             return View( await _context.Dishes.ToListAsync() );
         }
 
